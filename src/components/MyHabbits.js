fc14c2f964877ede37d29c32d224598c78f53985
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-04-27 12:21:09 +0200

    wip change child added to fetch3

--- src/components/MyHabbits.js
+++ src/components/MyHabbits.js
@@ -4,19 +4,21 @@ import { auth, dbRef, db } from "../setupFirebase"
 
 class MyHabbits extends React.Component {
   state = {
-    userHabbits:[]
+    userHabbits:[],
+    newHabbit: ''
   }
   getUserHabbits = (userId) => {
    
-   let x = getUserHabbits(userId)
-    // .then(userHabbits => {
-    //   this.setState({userHabbits})
-    // })
+   getUserHabbits(userId).then(habbits => 
+  this.setState({userHabbits: habbits})
+  )
+    
   }
 
-  componentDidMount(){
+  componentWillMount(){
     let userId = this.props.state.currentUserData.id
     this.getUserHabbits(userId)
+    console.log(this.props)
   }
  
 handleChange= (e) => {
@@ -25,19 +27,27 @@ handleChange= (e) => {
 handleSubmit = (e, userId) => {
   e.preventDefault()
   
-
-  db.ref(`users/${userId}/habbits/${Date.now()}` ).set(this.state.newHabbit);
+let id = Date.now()
+  db.ref(`users/${userId}/habbits/${id}` ).set({
+    name: this.state.newHabbit,
+    id: id
+  });
+  this.getUserHabbits(userId)
 
 }
-// deleteHabbit = (habbitId, userId) => {
-//   console.log(habbitId, userId)
-//   fetch(`https://obshab.firebaseio.com/users/${userId}/habbits/${habbitId}.json`, {
-//     method: "DELETE",
-//     headers: {
-//       "Content-Type": "application/json"
-//     }
-//   });
-// }
+handleDelete = (habbitId) => {
+
+}
+
+deleteHabbit = (habbitId, userId) => {
+  console.log(habbitId, userId)
+  fetch(`https://obshab.firebaseio.com/users/${userId}/habbits/${habbitId}.json`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+}
 
 
   render() {
@@ -53,9 +63,11 @@ handleSubmit = (e, userId) => {
        <button onClick ={(e) =>this.handleSubmit(e, userId)}>Add</button>
      
        </form>
+     {Object.values(this.state.userHabbits || {}).map(habbit => 
+       <li key={habbit.id}>{habbit.name} <button onClick={() =>this.handleDelete(habbit.id)}>DELETE</button></li>
+     )}
        
-       <button onClick={()=> console.log(this.state)}></button>
-        
+       
     </div>
   }
 }
--- src/service/fetching.js
+++ src/service/fetching.js
@@ -1,10 +1,9 @@
 const usersApiUrl = "https://obshab.firebaseio.com/";
 
-export const getUserHabbits = (userId) =>{
+export const getUserHabbits = (userId) =>
   fetch(usersApiUrl + `/users/${userId}/habbits.json`)
     .then(response => response.json())
     .then(userHabbits =>
-      Object.entries(userHabbits).map(x => x)
+        userHabbits
     );
 
-}
