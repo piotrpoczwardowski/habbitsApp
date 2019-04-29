7e5633c729518a0ce5ea4273cd2e2f254c2b484f
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-04-29 10:05:27 +0200

    add done when click on date

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,115 +1,149 @@
-
 import React from "react"
 
-import './Calendar.css'
+import "./Calendar.css"
 class Calendar extends React.Component {
   state = {
-    now: new Date()
-
+    now: new Date(),
+    userId: "",
+    habbit: "",
   }
-  componentWillMount(){
+  componentWillMount() {
     let props = this.props.location.state
-    this.setState({now: new Date(),
-    userId: props.userId,
-    habbitId: props.habbit.id
+    this.setState({
+      now: new Date(),
+      userId: props.userId,
+      habbit: props.habbit,
     })
-    
+    console.log(props)
   }
-changeYear = (e) => {
-  
-  let i = 0
-  if(e.target.innerHTML === 'Prev'){
-    i=-1
+  changeYear = e => {
+    let i = 0
+    if (e.target.innerHTML === "Prev") {
+      i = -1
+    }
+    if (e.target.innerHTML === "Next") {
+      i = 1
+    }
+
+    let now = this.state.now
+
+    now.setMonth(now.getMonth() + i)
+    this.setState({ now: now })
   }
-  if(e.target.innerHTML === 'Next'){
-    i=1
+
+  handleClick = (e, cellId) => {
+    console.log(cellId)
+    this.toggleDone(this.state.userId, this.state.habbit.id, cellId)
   }
+  toggleDone = (userId, habbitId, cellId) =>
   
-let now = this.state.now
-
-
-now.setMonth(now.getMonth()+ i)
-this.setState({now: now})
-}
-
-handleClick = (e, date) => {
- 
- this.toggleDone(this.state.userId, this.state.habbitId,date )
-}
-toggleDone = (userId, habbitId, date) => 
-  
-  fetch(`https://obshab.firebaseio.com/users/${userId}/habbits/${habbitId}/date/${date}.json`, {
-    method: "PUT",
-    body: JSON.stringify({
-      isDone: true
-      
-    }),
-    
-  })
-
-
+    fetch(
+      `https://obshab.firebaseio.com/users/${userId}/habbits/${habbitId}/date/${cellId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          isDone: true,
+        }),
+      }
+    )
 
   render() {
     let now = this.state.now
-    let day = now.getDate();
-    let month = now.getMonth();
-    let year = now.getFullYear();
-    const monthNames = ['styczeń', 'luty', 'marzec', 'kwiecień', 'maj', 'czerwiec', 'lipiec', 'sierpień', 'wrzesień', 'październik', 'listopad', 'grudzień'];
-    const days = ['Pon', 'Wto', 'Śro', 'Czw', 'Pią', 'Sob','Nie'];
-    const daysInMonth = new Date(year, month+1, 0).getDate();
-  const tempDate = new Date(year,month, 1);
-  let firstMonthDay = tempDate.getDay();
-  if (firstMonthDay === 0) {
-    firstMonthDay = 7;
-}
-const j = daysInMonth + firstMonthDay - 1;
-let allCells = []
-let dateCellsI = 1
-for(let i = 0; i<j; i++){
-  let cell 
-  if(i<firstMonthDay-1){
-    cell= ''
-  } else{
-    cell = new Date(year,month, (dateCellsI));
-    dateCellsI++
-  }
-  
-  
-  allCells.push(cell)
-}
-let chunkArray = []
-let chunk = 7
-for(let i=0; i<allCells.length; i+=chunk){
-  let part = allCells.slice(i, i+chunk)
-  chunkArray.push(part)
-}
+    let day = now.getDate()
+    let month = now.getMonth()
+    let year = now.getFullYear()
+    const monthNames = [
+      "styczeń",
+      "luty",
+      "marzec",
+      "kwiecień",
+      "maj",
+      "czerwiec",
+      "lipiec",
+      "sierpień",
+      "wrzesień",
+      "październik",
+      "listopad",
+      "grudzień",
+    ]
+    const days = ["Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie"]
+    const daysInMonth = new Date(year, month + 1, 0).getDate()
+    const tempDate = new Date(year, month, 1)
+    let firstMonthDay = tempDate.getDay()
+    if (firstMonthDay === 0) {
+      firstMonthDay = 7
+    }
+    const j = daysInMonth + firstMonthDay - 1
+    let allCells = []
+    let dateCellsI = 1
+    
+    for (let i = 0; i < j; i++) {
+      let cell
+      if (i < firstMonthDay - 1) {
+        cell = ""
+      } else {
+        cell = {date: new Date(year, month, dateCellsI)}
 
+        
+          
+        dateCellsI++
+      }
 
+      allCells.push(cell)
+    }
+    let chunkArray = []
+    let chunk = 7
+    for (let i = 0; i < allCells.length; i += chunk) {
+      let part = allCells.slice(i, i + chunk)
+      chunkArray.push(part)
+    }
 
+    let habbitDates = Object.entries(this.state.habbit.date).map(([id, isDone]) => ({
+      id,
+      ...isDone
+    }) )
 
-    return <div>
-
-    Calendar
-   
-    <div className="calendar">
-<div className="date__name">
-<button onClick={this.changeYear}>Prev</button>
-<span>{monthNames[month]} {year}</span>
-<button onClick={this.changeYear}>Next</button>
-</div>
-<div className="calendar__contener">
-<table>
-  <thead>
-  <tr>{days.map(x => <th>{x}</th> )}</tr>
-  </thead>
-  <tbody>
-  {chunkArray.map(chunk => <tr>{chunk.map(cell => <td  onClick={(e) => this.handleClick(e,cell)}>{cell&& cell.getDate()}</td>)}</tr>)}
-  </tbody>
-</table>
-</div>
-</div>
-
-    </div>
+    return (
+      <div>
+        
+        <div className="calendar">
+          <h1>Calendar</h1>
+          <h3>{this.state.habbit.name}</h3>
+          <div className="date__name">
+            <button onClick={this.changeYear}>Prev</button>
+            <span>
+              {monthNames[month]} {year}
+            </span>
+            <button onClick={this.changeYear}>Next</button>
+          </div>
+          <div className="calendar__contener">
+            <table>
+              <thead>
+                <tr>
+                  {days.map(x => (
+                    <th>{x}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {chunkArray.map(chunk => (
+                  <tr>
+                    {chunk.map(cell => (
+                      <td onClick={e => this.handleClick(e, cell.date)}
+                      className={habbitDates.some(x => x.id === `${cell.date}`)&& 'active'}
+                      >
+                        {cell && cell.date.getDate()}
+                     {console.log(habbitDates.some(x => x.id === `${cell.date}`))}
+                      </td>
+                    ))}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
