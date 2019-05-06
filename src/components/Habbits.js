b2982a92d9f3a5afee2d811f7a8178f7ababe6f3
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-05-06 22:26:39 +0200

    same working as calendar

--- src/components/Habbits.js
+++ src/components/Habbits.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { getUserHabbits, addHabbit, deleteHabbit } from "../service/fetching"
 import { auth, dbRef, db } from "../setupFirebase"
-import '../components/Habbits.css'
+import "../components/Habbits.css"
 
 class Habbits extends React.Component {
   state = {
@@ -9,8 +9,9 @@ class Habbits extends React.Component {
     userHabbits: [],
   }
   getUserHabbits = userId => {
-    getUserHabbits(userId).then(habbits =>
+    getUserHabbits(userId).then(habbits =>{
       this.setState({ userHabbits: habbits })
+    console.log(habbits)}
     )
   }
 
@@ -21,22 +22,33 @@ class Habbits extends React.Component {
     this.getUserHabbits(userId)
   }
   findHabbit = (habbit, day) => {
-    let habbitDate =  Object.entries(habbit.date || {}).map(([id, isDone])=> ({
+    let habbitDate = Object.entries(habbit.date || {}).map(([id, isDone]) => ({
       id,
-      ...isDone
+      ...isDone,
     }))
-    return (habbitDate.some(habbit => habbit.id === `${day.date}`&& habbit.isDone))
+    return habbitDate.some(
+      habbit => habbit.id === `${day.date}` && habbit.isDone
+    )
   }
-  handleClick = (e, cellId) => {
-    let isDone = e.target.classList.contains('done')
-    this.toggleDone(this.state.userId, this.state.habbit.id, cellId, isDone)
-    .then(()=> this.getHabbit())
+  handleClick = (e, cellId, habbitId) => {
+    let isDone = e.target.classList.contains("done")
+    this.toggleDone(this.state.userId, habbitId, cellId, isDone).then(() =>
+      this.getUserHabbits(this.state.userId)
+    )
   }
-  
+
+  toggleDone = (userId, habbitId, cellId, isDone) =>
+    fetch(
+      `https://obshab.firebaseio.com/users/${userId}/habbits/${habbitId}/date/${cellId}.json`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          isDone: !isDone,
+        }),
+      }
+    )
 
   render() {
-      
-  
     let sevenDays = []
     const monthNames = [
       "styczeń",
@@ -53,12 +65,12 @@ class Habbits extends React.Component {
       "grudzień",
     ]
     for (let i = -3; i < 4; i++) {
-      let day = {date:new Date()}
+      let day = { date: new Date() }
       day.date.setDate(day.date.getDate() + i)
-      day.date.setHours(0,0,0,0)
+      day.date.setHours(0, 0, 0, 0)
       sevenDays.push(day)
     }
-    
+
     return (
       <div>
         Habbitsaaaaaaaaaaaaaaaa
@@ -76,13 +88,17 @@ class Habbits extends React.Component {
           <tbody>
             {this.state.userHabbits.map(habbit => (
               <tr key={habbit.id}>
-              <td>{habbit.name}</td>
-              {sevenDays.map(day => (
-                <td onClick={e => this.handleClick(e, cell.date)} className={this.findHabbit(habbit, day) ? 'done': undefined}>
-                  {day.date.getDate()}  {monthNames[day.date.getMonth()]}
-                 
-                </td>
-              ))}
+                <td>{habbit.name}</td>
+                {sevenDays.map(day => (
+                  <td
+                    onClick={e => this.handleClick(e, day.date, habbit.id)}
+                    className={
+                      this.findHabbit(habbit, day) ? "done" : undefined
+                    }
+                  >
+                    {day.date.getDate()} {monthNames[day.date.getMonth()]}
+                  </td>
+                ))}
               </tr>
             ))}
           </tbody>
