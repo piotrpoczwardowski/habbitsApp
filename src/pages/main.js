32b9c49c1154fe33e716bdecc7a0d688765b57f6
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-05-07 21:49:17 +0200

    wip styled main

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,8 +2,7 @@ import React from "react"
 import { navigate } from "gatsby"
 import { auth, dbRef, db } from "../setupFirebase"
 import EditHabbits from "../components/EditHabbits"
-import Habbits from '../components/Habbits'
-
+import Habbits from "../components/Habbits"
 
 class main extends React.Component {
   state = {
@@ -12,7 +11,7 @@ class main extends React.Component {
     currentUserData: {},
     isLoading: true,
     userHabbits: [],
-    component: 'Habbits'
+    component: "Habbits",
   }
   componentWillMount() {
     auth.onAuthStateChanged(firebaseUser => {
@@ -36,43 +35,40 @@ class main extends React.Component {
     })
   }
 
-
-
   logout = () => {
     auth.signOut()
     navigate("/")
   }
   show = () => {
     console.log(Object.values(this.state.userHabbits).map(x => x.key))
-
   }
-  changePage = (e) => {
-    console.log(e.target.innerHTML)
-    this.setState({component: e.target.innerHTML})
+  changePage = e => {
+    this.setState({ component: e.target.innerHTML })
   }
 
   render() {
     let components = {
-      'Habbits': <Habbits state={this.state}/>,
-      'EditHabbits': <EditHabbits state={this.state}/>
+      Habbits: <Habbits state={this.state} />,
+      EditHabbits: <EditHabbits state={this.state} />,
     }
     return (
-      <div>
-        <button onClick={this.logout}>Logout</button>
-       
+      <div className='main'>
+        <div className="navigation">
+          <button className='btn btn__nav' onClick={this.changePage}>Habbits</button>{" "}
+          <button className='btn btn__nav' onClick={this.changePage}>EditHabbits</button>
+          <button className="btn btn__logout" onClick={this.logout}>
+            Logout
+          </button>
+        </div>
+
         <p>{this.state.user.email}</p>
-        <p>{this.state.currentUserData.nick}</p>
-<div><span onClick={this.changePage}>Habbits</span> <span onClick={this.changePage}>EditHabbits</span></div>
+
+        <div />
         {this.state.isLoading ? (
           "load"
         ) : (
-          <div>
-           
-           {components[this.state.component]}
-          </div>
+          <div>{components[this.state.component]}</div>
         )}
-
-       
       </div>
     )
   }
