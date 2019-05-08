89b1c221cab03d11ca046f544439e26646550484
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-05-08 13:03:39 +0200

    change calendar to component

--- src/components/EditHabbits.js
+++ src/components/EditHabbits.js
@@ -1,14 +1,15 @@
 import React from "react"
 import { getUserHabbits, addHabbit, deleteHabbit } from "../service/fetching"
 import { auth, dbRef, db } from "../setupFirebase"
-import {Link} from 'gatsby'
-import Fade from 'react-reveal/Fade';
+import { Link } from "gatsby"
+import Fade from "react-reveal/Fade"
 class EditHabbits extends React.Component {
   state = {
     userHabbits: [],
     newHabbit: "",
     userId: "",
-    isPositive: 'true',
+    isPositive: "true",
+    component: "EditHabbits",
   }
   getUserHabbits = userId => {
     getUserHabbits(userId).then(habbits =>
@@ -50,26 +51,45 @@ class EditHabbits extends React.Component {
   render() {
     let userId = this.props.state.currentUserData.id
     let userHabbits = this.props.state.userHabbits
-   
+
     return (
-      <div className='editHabbits'>
-        <form action="">
-          <select className='btn' onChange={this.selectChange}>
-            <option value={true}>Positiv</option>
-            <option value={false}>Negativ</option>
-          </select>
-          <input className='inp' onChange={this.handleChange} type="text" />
-          <button className='btn' onClick={e => this.handleSubmit(e)}>Add</button>
-        </form>
-        <div className="editHabbits__grid">
-        {this.state.userHabbits.map(habbit => (
-          <li key={habbit.id}>
-            {habbit.name}
-            <button onClick={() => this.handleDelete(habbit.id)}>X</button>
-            <button className='btn'><Link state={{userId:this.state.userId,
-            habbit: habbit }} to='/Calendar'> Calendar</Link></button>
-          </li>
-        ))}
+      <div>
+        <div className="editHabbits">
+          <form action="">
+            <select className="btn" onChange={this.selectChange}>
+              <option value={true}>Positiv</option>
+              <option value={false}>Negativ</option>
+            </select>
+            <input className="inp" onChange={this.handleChange} type="text" />
+            <button className="btn" onClick={e => this.handleSubmit(e)}>
+              Add
+            </button>
+          </form>
+          <div className="editHabbits__list">
+            {this.state.userHabbits.map(habbit => (
+              <div className="habbit">
+                <div className="habbit__name">{habbit.name}</div>
+                <div className="habbit__calendar">
+                  <button className="btn2"><Link state={{userId:this.state.userId,
+                habbit: habbit }} to='/Calendar'> Calendar</Link></button>
+                </div>
+                <div className="habbit__delete">
+                  <button
+                    onClick={() => this.handleDelete(habbit.id)}
+                    className="btn2"
+                  >
+                    X
+                  </button>
+                </div>
+              </div>
+              // <div className='habbit' key={habbit.id}>
+              //  <p>{habbit.name}</p>
+              //   <button className='btn btn-delete' onClick={() => this.handleDelete(habbit.id)}>X</button>
+              //   <button className='btn'><Link state={{userId:this.state.userId,
+              //   habbit: habbit }} to='/Calendar'> Calendar</Link></button>
+              // </div>
+            ))}
+          </div>
         </div>
       </div>
     )
