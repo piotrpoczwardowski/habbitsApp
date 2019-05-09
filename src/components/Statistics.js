fa8edbb174d47632534ba9cb83ef73044441d1d7
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-05-09 11:04:57 +0200

    wip statistics

---
+++ src/components/Statistics.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import { auth, dbRef, db } from "../setupFirebase"
+
+class name extends React.Component {
+  state = {}
+
+  render() {
+    return <div />
+  }
+}
+
+export default name
--- src/pages/main.js
+++ src/pages/main.js
@@ -3,8 +3,8 @@ import { navigate } from "gatsby"
 import { auth, dbRef, db } from "../setupFirebase"
 import EditHabbits from "../components/EditHabbits"
 import Habbits from "../components/Habbits"
-import Fade from 'react-reveal/Fade';
-import Calendar from '../components/Calendar'
+import Fade from "react-reveal/Fade"
+import Calendar from "../components/Calendar"
 
 class main extends React.Component {
   state = {
@@ -14,7 +14,7 @@ class main extends React.Component {
     isLoading: true,
     userHabbits: [],
     component: "Habbits",
-    propHabbit: ''
+    propHabbit: "",
   }
   componentWillMount() {
     auth.onAuthStateChanged(firebaseUser => {
@@ -26,7 +26,7 @@ class main extends React.Component {
           .then(x =>
             Object.values(x).map(user => {
               if (user.email === firebaseUser.email) {
-                this.setState({ currentUserData: user, isLoading: false, })
+                this.setState({ currentUserData: user, isLoading: false })
               }
             })
           )
@@ -46,37 +46,50 @@ class main extends React.Component {
     console.log(Object.values(this.state.userHabbits).map(x => x.key))
   }
   changePage = (e, habbit) => {
-    this.setState({ component: e.target.innerHTML, propHabbit: habbit  })
-    
+    this.setState({ component: e.target.innerHTML, propHabbit: habbit })
   }
 
   render() {
     let components = {
-      Habbits: <Fade><Habbits state={this.state} /></Fade>,
-      EditHabbits: <EditHabbits changePage={this.changePage} state={this.state} />,
-      Calendar: <Calendar userId={this.state.currentUserData.id} habbit={this.state.propHabbit}/>
+      Habbits: (
+        
+          <Habbits state={this.state} />
+       
+      ),
+      EditHabbits: (
+        <EditHabbits changePage={this.changePage} state={this.state} />
+      ),
+      Calendar: (
+        <Calendar
+          userId={this.state.currentUserData.id}
+          habbit={this.state.propHabbit}
+        />
+      ),
     }
     return (
-      <div className='main'>
-      {console.log(this.state)}
+      <div className="main">
         <div className="navigation">
-          <button className='btn btn__nav' onClick={this.changePage}>Habbits</button>{" "}
-          <button className='btn btn__nav' onClick={this.changePage}>EditHabbits</button>
+          <button className="btn btn__nav" onClick={this.changePage}>
+            Habbits
+          </button>
+          <button className="btn btn__nav" onClick={this.changePage}>
+            Statistics
+          </button>
+          <button className="btn btn__nav" onClick={this.changePage}>
+            EditHabbits
+          </button>
           <button className="btn btn__logout" onClick={this.logout}>
             Logout
           </button>
         </div>
-        
 
         <p>{this.state.user.email}</p>
 
-       
         {this.state.isLoading ? (
           "load"
         ) : (
           <div>{components[this.state.component]}</div>
         )}
-        
       </div>
     )
   }
