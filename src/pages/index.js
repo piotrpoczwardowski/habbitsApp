4d27c9dfef4c5722d2b81dad5d10834275c24a17
piotrpoczwardowski <piotrpoczwardowski@gmail.com>
2019-05-14 22:13:39 +0200

    start styled component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
 import { auth, dbRef, db } from "../setupFirebase"
 import Login from "../components/Login"
 import { navigate } from "gatsby"
 import './index.scss'
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import TransitionLink from 'gatsby-plugin-transition-link'
+import styled from "styled-components"
+import {Section} from '../styles/style'
+
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
 
 class index extends React.Component {
   componentDidMount() {
@@ -26,13 +32,16 @@ class index extends React.Component {
     navigate("/main/")
   }
 
+  
+
   render() {
+    
     return (
-      <div>
+      <Section>
        
         <Login />
        
-      </div>
+      </Section>
     )
   }
 }
---
+++ src/styles/style.js
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+
+export const Title = styled.h1`
+font-size: 2em;
+text-align: center;
+color: palevioletred;
+`;
+
+export const Section = styled.div`
+background: black;
+`
